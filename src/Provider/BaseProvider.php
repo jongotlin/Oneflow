d7dae9e6023b101fc6f825f75843bc0227625ae7
Jon Gotlin <jon@jon.se>
2023-10-22 18:26:49 +0200

    Upload file (#12)

--- src/Provider/BaseProvider.php
+++ src/Provider/BaseProvider.php
@@ -5,6 +5,8 @@ namespace JGI\Oneflow\Provider;
 use JGI\Oneflow\Credentials;
 use JGI\Oneflow\Exception\OneflowException;
 use JGI\Oneflow\Oneflow;
+use Symfony\Component\Mime\Part\DataPart;
+use Symfony\Component\Mime\Part\Multipart\FormDataPart;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 abstract class BaseProvider implements ProviderInterface
@@ -60,6 +62,32 @@ abstract class BaseProvider implements ProviderInterface
                 'json' => $data,
             ], $this->createOptions())
         );
+
+        $json = $response->getContent(false);
+
+        $code = $response->getStatusCode();
+        if ($code && 200 != $code) {
+            throw new OneflowException($json);
+        }
+
+        $data = json_decode($json, true);
+        if (null === $data) {
+            throw new OneflowException('Content is not json: '.$json);
+        }
+
+        return $data;
+    }
+
+    protected function postFile(string $path, \SplFileInfo $file): ?array
+    {
+        $options = $this->createOptions(false);
+
+        $options['body'] = [
+            'upload_as' => 'expanded_pdf',
+            'file' => fopen($file->getRealPath(), 'r'),
+        ];
+
+        $response = $this->client->request('POST', $this->getUrl($path), $options);
 
         $json = $response->getContent(false);
 
